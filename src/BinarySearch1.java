5980f97cad1a4f7050c2d8aa7ac912e1d4fea43f
megavathharitha <megavathharitha819@gmail.com>
2023-11-25 23:06:02 +0530

    Binary search

--- src/BinarySearch1.java
+++ src/BinarySearch1.java
@@ -1,5 +1,5 @@
 //Time complexity --O(log n)
-public class BinarySearch {
+public class BinarySearch1 {
 
     public static void main(String[] args) {
         int arr[] = {10,20,30,40,50,60,70,80,90};
