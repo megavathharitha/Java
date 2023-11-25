cef99f9e647c016926fd2ee9ae0f41e47ff52de1
megavathharitha <megavathharitha819@gmail.com>
2023-11-25 22:45:23 +0530

    "Binary Search"

---
+++ src/BinarySearch.java
@@ -0,0 +1,28 @@
+//Time complexity --O(log n)
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50,60,70,80,90};
+        int x = 50;
+        binarySearch(arr,x,0,arr.length-1);
+
+    }
+    static void binarySearch(int arr[],int x,int start,int end){
+        if(arr.length==0){
+            return;
+        }
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==x){
+                System.out.println(mid);
+                return;
+            }
+            else if(arr[mid]<x){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+    }
+}
