d538a2c1cc2269edc90e2c6c6f3ef10c459fc502
megavathharitha <megavathharitha819@gmail.com>
2023-11-26 15:24:49 +0530

    "Linear Search"

---
+++ src/LinearSearch.java
@@ -0,0 +1,12 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int arr[] = {33,56,7,8,9};
+        int n = arr.length;
+        int target = 56;
+        for(int i = 0;i<n;i++){
+            if(arr[i]==target){
+                System.out.println(i);
+            }
+        }
+    }
+}
